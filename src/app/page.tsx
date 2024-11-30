1a1d581aa786998a01d920da4a42ffb3d4209cc7
risv1 <rishi.aangarai@gmail.com>
2024-11-30 12:43:39 +0530

    feat: add metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Image from "next/image";
+
 const Home: React.FC = () => {
   return (
-    <main className="w-screen h-screen flex flex-col gap-3 items-center">
-      <h1 className="text-4xl font-bold text-center text-black dark:text-white mt-32">NPFL</h1>
+    <main className="w-screen h-screen flex flex-col gap-5 items-center">
+      <h1 className="flex flex-row items-center text-4xl font-bold text-center text-black dark:text-white mt-32">
+        <Image src="/images/logo.png" alt="NPFL" width={70} height={70} />
+        NPFL
+      </h1>
       <p className="text-center dark:text-neutral-400 text-neutral-800">Coming soon...</p>
     </main>
   );
